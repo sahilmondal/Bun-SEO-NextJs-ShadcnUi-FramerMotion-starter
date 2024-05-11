a415ce14afd9602e45542208bf417025e2b3dda5
Sahil Mondal <development.sahil@gmail.com>
2024-05-11 21:51:59 +0530

    v1.0

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
+const currentUrl = process.env.NEXT_PUBLIC_WEBSITE_URL as string;
 export const siteConfig = {
   title: "Next-tailwind-shadcn-motion-SEO-template",
   siteName: "Site Name",
   description: "Meta description",
-  url: process.env.NEXT_PUBLIC_WEBSITE_URL as string,
+  url: currentUrl ? currentUrl : "http://localhost:3000",
   keywords: ["tags", "tags"],
   logoURL: "/assets/logo.png",
   ogImage: "/assets/og.png",
